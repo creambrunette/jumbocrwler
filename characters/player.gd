2c84fd3bfa4d551de459029a3f13f5f3d886b111
creambrunette <63561471+creambrunette@users.noreply.github.com>
2023-07-15 11:22:59 +0200

    my first commit

---
+++ characters/player.gd
@@ -0,0 +1,22 @@
+extends CharacterBody2D
+
+@export var speed = 400
+@onready var anim = $AnimationPlayer
+
+func get_input():
+	var input_direction = Input.get_vector("left", "right", "up", "down")
+	if input_direction[0] == 1:
+		anim.play("side")
+		$AnimatedSprite2D.flip_h = true
+	elif input_direction[0] == -1:
+		anim.play("side")
+		$AnimatedSprite2D.flip_h = false
+	elif input_direction[1] == 1:
+		anim.play("front")
+	elif input_direction[1] == -1:
+		anim.play("back")
+	velocity = input_direction * speed
+
+func _physics_process(delta):
+	get_input()
+	move_and_slide()
